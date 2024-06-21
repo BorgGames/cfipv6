c932d8397d1f8b24b987705324e5bce016ce97c0
Victor Nova <victor@borg.games>
2024-06-20 21:52:36 -0700

    enabled configuration

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using cfipv6;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.Configure<AddressUpdaterOptions>(builder.Configuration.GetSection("AddressUpdater"));
 builder.Services.AddHostedService<AddressUpdater>();
 
 var host = builder.Build();
