a1bcffd6695f4f27e24716c08e724661bba33afc
Victor Nova <victor@borg.games>
2024-06-20 23:10:25 -0700

    correctly construct URL; also added logging and reserved domain field

--- AddressUpdater.cs
+++ AddressUpdater.cs
@@ -11,6 +11,7 @@ public sealed class AddressUpdater: IHostedService {
     readonly HttpClient zonesAPI;
     readonly string zoneID;
     readonly string recordID;
+    readonly string domain;
     readonly ILogger<AddressUpdater> log;
 
     public AddressUpdater(IOptions<AddressUpdaterOptions> options, ILogger<AddressUpdater> log) {
@@ -18,7 +19,7 @@ public sealed class AddressUpdater: IHostedService {
         this.log = log ?? throw new ArgumentNullException(nameof(log));
 
         this.zonesAPI = new() {
-            BaseAddress = new("https://api.cloudflare.com/client/v4/zones"),
+            BaseAddress = new("https://api.cloudflare.com/client/v4/zones/"),
             DefaultRequestHeaders = {
                 Authorization = new("Bearer", opt.ApiKey),
             },
@@ -26,9 +27,11 @@ public sealed class AddressUpdater: IHostedService {
         this.zonesAPI.DefaultRequestHeaders.Add("X-Auth-Email", opt.Email);
         this.zoneID = opt.ZoneID ?? throw new ArgumentNullException(nameof(opt.ZoneID));
         this.recordID = opt.RecordID ?? throw new ArgumentNullException(nameof(opt.RecordID));
+        this.domain = opt.Domain ?? throw new ArgumentNullException(nameof(opt.Domain));
     }
 
     async Task OnAddressChanged() {
+        string? responseContent = null;
         try {
             var address = IP.GetPublicStableIPv6();
             if (address is null) {
@@ -36,19 +39,22 @@ public sealed class AddressUpdater: IHostedService {
                 return;
             }
 
+            // TODO: check domain name
+
             var response = await this.zonesAPI.PatchAsync(
                 $"{this.zoneID}/dns_records/{this.recordID}",
                 new StringContent(JsonSerializer.Serialize(new {
                     type = "AAAA",
-                    name = "example.com",
                     content = address.Address.ToString(),
-                    ttl = 120,
                 }), Encoding.UTF8, "application/json")
             ).ConfigureAwait(false);
 
+            if (response.Content is not null)
+                responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
             response.EnsureSuccessStatusCode();
+            this.log.LogInformation("Updated {Domain} to {Address}", this.domain, address.Address);
         } catch (Exception e) {
-            this.log.LogError(e, "Failed to update IP address: {Error}", e);
+            this.log.LogError(e, "Failed to update IP address: {Error} {Reason}", e, responseContent);
         }
     }
 
