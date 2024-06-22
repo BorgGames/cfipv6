6cd2de637589522a0d7b49dd59e99ccdcc346b7e
Victor Nova <victor@borg.games>
2024-06-21 23:42:28 -0700

    fixed: ignore unique local IPv6 addresses

--- IP.cs
+++ IP.cs
@@ -11,7 +11,10 @@ public static class IP {
 
             foreach (var address in adapter.GetIPProperties().UnicastAddresses) {
                 if (address.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6) {
-                    if (address.Address.IsIPv6LinkLocal || address.Address.IsIPv6SiteLocal || IPAddress.IsLoopback(address.Address))
+                    if (address.Address.IsIPv6LinkLocal
+                        || address.Address.IsIPv6SiteLocal
+                        || address.Address.IsIPv6UniqueLocal
+                        || IPAddress.IsLoopback(address.Address))
                         continue;
 
 #warning TODO: filter out temporary addresses
